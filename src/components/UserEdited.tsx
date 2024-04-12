da1ed20c9f1d2eb0ea0e51a873a7ae58a1a7f2ca
KevinPgn <kevin.pigeon2810@gmail.com>
2024-04-12 22:04:47 +0200

    crud users

--- src/components/UserEdited.tsx
+++ src/components/UserEdited.tsx
@@ -3,15 +3,34 @@ import { useRouter } from "next/navigation"
 import { Button } from "./ui/button"
 import { Input } from "./ui/input"
 import { updateUserDB } from "@/server/Actions"
+import { ToastContainer, toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 export const UserEdited = ({user}: any) => {
   const router = useRouter()
+
+  const handleAction = (formData: FormData) => {
+    const name = formData.get('name') as string
+    const email = formData.get('email') as string
+    const number = formData.get('number') as string
+    
+    if (name.length < 3) return toast.error('Name must be at least 3 characters long!')
+    if (!email.includes('@')) return toast.error('Invalid email!')
+    if (number.length !== 10) return toast.error('Phone number must be 10 characters long!')
+    
+    try {
+      toast.success('User updated successfully!')
+      updateUserDB(user.id, formData)
+    } catch (error) {
+      toast.error('Failed to update user!')
+    }
+  }
 
   return <>
     <h1 className="text-2xl text-white text-center mb-5 mt-5">Edit User</h1>
     <form
       className="flex flex-col mx-auto max-w-2xl gap-5"
-      action={() => updateUserDB(user.id, new FormData)}>
+      action={handleAction}>
       <Input
         className="bg-transparent border-b-2 border-gray-300 text-[17px] text-white p-7"
         defaultValue={user.name}
@@ -38,5 +57,7 @@ export const UserEdited = ({user}: any) => {
         <Button onClick={() => router.push('/')} className="w-[100px]" variant="destructive" type="reset">Cancel</Button>
       </div>
     </form>
+
+    <ToastContainer />
   </> 
 }
